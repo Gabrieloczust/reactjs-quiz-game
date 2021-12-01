5aa9703b7ff01b0626acb9d3bbb00328c357ac3a
Gabriel Oczust <gabriel.oczust@gmail.com>
2021-11-30 23:09:04 -0300

    fix(services): return only questions in getQuestiions

--- src/services/questions/index.js
+++ src/services/questions/index.js
@@ -2,7 +2,7 @@ import { api } from 'services/api'
 
 export const getQuestions = async (amount) => {
   try {
-    return await api.get('/', { params: { amount: Number(amount) } })
+    return api.get('/', { params: { amount: Number(amount) } }).then(response => response.data.results)
   } catch {
     return []
   }
